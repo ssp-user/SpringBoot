9627f904d2a53b266e6f167a091b75523ccc01e9
vishaljoshi <vishaljoshi@N-MacBookPro-08.local>
2022-11-21 11:39:21 +0530

    First commit

---
+++ src/main/java/com/example/demo/MySpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// dummy comments
+@SpringBootApplication
+public class MySpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootProjectApplication.class, args);
+	}
+
+}
